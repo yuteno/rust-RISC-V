3ab4e214af0e18c1de5c7dc7965a68b538c4fc85
Yosuke Ueno <youten622@yahoo.co.jp>
2020-05-03 12:44:45 +0900

    debug of store instructions

--- src/cpu/base.rs
+++ src/cpu/base.rs
@@ -2,13 +2,12 @@ use super::Cpu;
 //memory size = 128MiB
 pub const MEMORY_SIZE: u64 = 1024 * 1024 * 128;
 
-
 impl Cpu {
     pub fn new(binary: Vec<u8>) -> Self {
         let mut memory = vec![0; MEMORY_SIZE as usize];
         memory.splice(..binary.len(), binary.iter().cloned());
 
-        let mut regs = [0;32];
+        let mut regs = [0; 32];
         regs[2] = MEMORY_SIZE;
         Self {
             regs,
@@ -28,7 +27,6 @@ impl Cpu {
         self.memory.len() as u64
     }
 
-
     pub fn dump_registers(&self) {
         let mut output = String::from("");
         for i in (0..32).step_by(4) {
@@ -40,11 +38,11 @@ impl Cpu {
                     i,
                     self.regs[i],
                     i + 1,
-                    self.regs[i+1],
+                    self.regs[i + 1],
                     i + 2,
-                    self.regs[i+2],
+                    self.regs[i + 2],
                     i + 3,
-                    self.regs[i+3],
+                    self.regs[i + 3],
                 )
             );
         }
@@ -61,58 +59,57 @@ impl Cpu {
 
     pub(crate) fn read16(&self, addr: u64) -> u64 {
         let index = addr as usize;
-        return (self.memory[index] as u64) | ((self.memory[index+1] as u64) << 8);
+        return (self.memory[index] as u64) | ((self.memory[index + 1] as u64) << 8);
     }
 
     pub(crate) fn read32(&self, addr: u64) -> u64 {
         let index = addr as usize;
         return (self.memory[index] as u64)
-        | ((self.memory[index+1] as u64) << 8)
-        | ((self.memory[index+2] as u64) << 16)
-        | ((self.memory[index+3] as u64) << 24);
+            | ((self.memory[index + 1] as u64) << 8)
+            | ((self.memory[index + 2] as u64) << 16)
+            | ((self.memory[index + 3] as u64) << 24);
     }
 
     pub(crate) fn read64(&self, addr: u64) -> u64 {
         let index = addr as usize;
         return (self.memory[index] as u64)
-        | ((self.memory[index+1] as u64) << 8)
-        | ((self.memory[index+2] as u64) << 16)
-        | ((self.memory[index+3] as u64) << 24)
-        | ((self.memory[index+4] as u64) << 32)
-        | ((self.memory[index+5] as u64) << 40)
-        | ((self.memory[index+6] as u64) << 48)
-        | ((self.memory[index+7] as u64) << 56)
+            | ((self.memory[index + 1] as u64) << 8)
+            | ((self.memory[index + 2] as u64) << 16)
+            | ((self.memory[index + 3] as u64) << 24)
+            | ((self.memory[index + 4] as u64) << 32)
+            | ((self.memory[index + 5] as u64) << 40)
+            | ((self.memory[index + 6] as u64) << 48)
+            | ((self.memory[index + 7] as u64) << 56);
     }
 
-
-    pub(crate) fn write8(&mut self, addr: u64, val: u64)  {
+    pub(crate) fn write8(&mut self, addr: u64, val: u64) {
         let index = addr as usize;
         self.memory[index] = val as u8;
     }
 
-    pub(crate) fn write16(&mut self, addr: u64, val: u64)  {
+    pub(crate) fn write16(&mut self, addr: u64, val: u64) {
         let index = addr as usize;
         self.memory[index] = (val & 0xff) as u8;
-        self.memory[index+1] = ((val >> 8) & 0xff) as u8;
+        self.memory[index + 1] = ((val >> 8) & 0xff) as u8;
     }
 
-    pub(crate) fn write32(&mut self, addr: u64, val: u64)  {
+    pub(crate) fn write32(&mut self, addr: u64, val: u64) {
         let index = addr as usize;
         self.memory[index] = (val & 0xff) as u8;
-        self.memory[index+1] = ((val >> 8) & 0xff) as u8;
-        self.memory[index+2] = ((val >> 16) & 0xff) as u8;
-        self.memory[index+3] = ((val >> 24) & 0xff) as u8;
+        self.memory[index + 1] = ((val >> 8) & 0xff) as u8;
+        self.memory[index + 2] = ((val >> 16) & 0xff) as u8;
+        self.memory[index + 3] = ((val >> 24) & 0xff) as u8;
     }
 
-    pub(crate) fn write64(&mut self, addr: u64, val: u64)  {
+    pub(crate) fn write64(&mut self, addr: u64, val: u64) {
         let index = addr as usize;
         self.memory[index] = (val & 0xff) as u8;
-        self.memory[index+1] = ((val >> 8) & 0xff) as u8;
-        self.memory[index+2] = ((val >> 16) & 0xff) as u8;
-        self.memory[index+3] = ((val >> 24) & 0xff) as u8;
-        self.memory[index+4] = ((val >> 32) & 0xff) as u8;
-        self.memory[index+5] = ((val >> 40) & 0xff) as u8;
-        self.memory[index+6] = ((val >> 48) & 0xff) as u8;
-        self.memory[index+7] = ((val >> 56) & 0xff) as u8;
+        self.memory[index + 1] = ((val >> 8) & 0xff) as u8;
+        self.memory[index + 2] = ((val >> 16) & 0xff) as u8;
+        self.memory[index + 3] = ((val >> 24) & 0xff) as u8;
+        self.memory[index + 4] = ((val >> 32) & 0xff) as u8;
+        self.memory[index + 5] = ((val >> 40) & 0xff) as u8;
+        self.memory[index + 6] = ((val >> 48) & 0xff) as u8;
+        self.memory[index + 7] = ((val >> 56) & 0xff) as u8;
     }
 }
--- src/cpu/execute.rs
+++ src/cpu/execute.rs
@@ -1,8 +1,6 @@
 use super::Cpu;
 
-
 impl Cpu {
-
     pub fn execute(&mut self, inst: u32) -> bool {
         let opcode = inst & 0x0000007f;
         let rd = ((inst & 0x00000f80) >> 7) as usize;
@@ -12,6 +10,7 @@ impl Cpu {
         let funct7 = (inst & 0xfe000000) >> 25;
         self.regs[0] = 0;
 
+        dbg!(format!("now executing: opcode {:#x}", opcode));
         match opcode {
             0x03 => {
                 let imm = ((inst as i32 as i64) >> 20) as u64;
@@ -59,7 +58,6 @@ impl Cpu {
                         self.regs[rd] = val;
                     }
                     _ => {}
-
                 }
             }
 
@@ -93,11 +91,7 @@ impl Cpu {
 
                     0x3 => {
                         //sltiu
-                        self.regs[rd] = if self.regs[rs1] < imm {
-                            1
-                        } else {
-                            0
-                        };
+                        self.regs[rd] = if self.regs[rs1] < imm { 1 } else { 0 };
                     }
 
                     0x4 => {
@@ -160,12 +154,14 @@ impl Cpu {
                         match funct7 {
                             0x00 => {
                                 //srliw
-                                self.regs[rd] = (self.regs[rs1] as u32).wrapping_shr(shamt) as i32 as i64 as u64;
+                                self.regs[rd] = (self.regs[rs1] as u32).wrapping_shr(shamt) as i32
+                                    as i64 as u64;
                             }
 
                             0x20 => {
                                 //sraiw
-                                self.regs[rd] = (self.regs[rs1] as i32).wrapping_shr(shamt) as i64 as u64;
+                                self.regs[rd] =
+                                    (self.regs[rs1] as i32).wrapping_shr(shamt) as i64 as u64;
                             }
 
                             _ => {}
@@ -177,11 +173,12 @@ impl Cpu {
             }
 
             0x23 => {
-                let imm = (((inst & 0xfe00000) as i32 as i64 >> 20) as u64) | (((inst >> 7) & 0x1f)as u64);
+                let imm = (((inst & 0xfe000000) as i32 as i64 >> 20) as u64)
+                    | (((inst >> 7) & 0x1f) as u64);
                 let addr = self.regs[rs1].wrapping_add(imm);
 
                 match funct3 {
-                    0x0 => self.write8(addr, self.regs[rs2]), // sb
+                    0x0 => self.write8(addr, self.regs[rs2]),  // sb
                     0x1 => self.write16(addr, self.regs[rs2]), // sh
                     0x2 => self.write32(addr, self.regs[rs2]), // sw
                     0x3 => self.write64(addr, self.regs[rs2]), // sd
@@ -217,7 +214,7 @@ impl Cpu {
 
                     (0x3, 0x00) => {
                         //sltu
-                        self.regs[rd] = if self.regs[rs1] < self.regs[rs2]{
+                        self.regs[rd] = if self.regs[rs1] < self.regs[rs2] {
                             1
                         } else {
                             0
@@ -262,12 +259,14 @@ impl Cpu {
                 match (funct3, funct7) {
                     (0x0, 0x00) => {
                         //addw
-                        self.regs[rd] = self.regs[rs1].wrapping_add(self.regs[rs2]) as i32 as i64 as u64;
+                        self.regs[rd] =
+                            self.regs[rs1].wrapping_add(self.regs[rs2]) as i32 as i64 as u64;
                     }
 
                     (0x0, 0x20) => {
                         //subw
-                        self.regs[rd] = ((self.regs[rs1].wrapping_sub(self.regs[rs2])) as i32) as u64;
+                        self.regs[rd] =
+                            ((self.regs[rs1].wrapping_sub(self.regs[rs2])) as i32) as u64;
                     }
 
                     (0x1, 0x00) => {
@@ -293,7 +292,7 @@ impl Cpu {
                 let imm = (((inst & 0x80000000) as i32 as i64 >> 19) as u64)
                     | (((inst & 0x80) << 4) as u64)
                     | (((inst >> 20) & 0x7e0) as u64)
-                    | (((inst >> 7) & 0x1e) as u64) ;
+                    | (((inst >> 7) & 0x1e) as u64);
 
                 match funct3 {
                     0x0 => {
